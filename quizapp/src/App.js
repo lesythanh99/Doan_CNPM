383cde1d712c187d0cfa076469bcfa13905fbe2b
nyrmal <taan55374@gmail.com>
2021-03-09 21:44:01 +0700

    add

--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home.js';
-import Intructions from './components/Quiz/intructions';
+import quizInstructions from './components/Quiz/intructions';
 import Play from './components/Quiz/Play';
+import QuizSummary from './components/Quiz/QuizSummary';
 
 
 function App() {
   return (
     <Router>
         <Route path="/" exact component = {Home}/>
-        <Route path="/play/huongdan" component = {Intructions}/>
+        <Route path="/play/huongdan" component = {quizInstructions}/>
         <Route path="/play/quiz" component = {Play}/>
+        <Route path="/play/quizSummary" component = {QuizSummary}/>
     </Router>
   );
 }
