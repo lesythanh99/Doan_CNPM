5206167a918ea9f01df22b89387666dd5d4a1ae7
Nguyen Cong Nhat <51161694+ckan719@users.noreply.github.com>
2021-03-14 16:52:09 +0700

    fix

--- quizapp/src/components/Etest/test.js
+++ quizapp/src/components/Etest/test.js
@@ -5,6 +5,18 @@ import { Container, Row, Col, Button } from 'reactstrap';
 function Test() {
     const [listItems, setListItems] = React.useState([]);
     const [activeNow, setActiveNow] = React.useState(0);
+    const [yourChoose, setYourChoose] = React.useState({
+        '0': '',
+        '1': '',
+        '2': '',
+        '3': '',
+        '4': '',
+        '5': '',
+        '6': '',
+        '7': '',
+        '8': '',
+        '9': '',
+    });
     const notifyData = () => {
         CRUD.getQuestion().then((res) => {
             setListItems(res.data.data);
@@ -13,7 +25,7 @@ function Test() {
     }
     const isActive = (index) => {
         if (index === activeNow) {
-            return 'ques active';
+            return 'ques activee';
         }
         return 'ques';
     }
@@ -27,6 +39,23 @@ function Test() {
         if (index < listItems.length - 1) setActiveNow(index + 1);
         return;
     }
+    const isChoose = (index, choose) => {
+        var data = { ...yourChoose };
+        data[index] = choose;
+        setYourChoose(data);
+    }
+    const submitTest = () => {
+        console.log(yourChoose);
+        var point = 0;
+        listItems.map((item, index) => {
+            if (item.ansCorrect === yourChoose[index]) point++;
+        });
+        console.log(point);
+    }
+    const isCAT = (index, ans) => {
+        if (yourChoose[index] === ans) return 'success';
+        return 'primary'
+    }
     React.useEffect(() => {
         notifyData();
     }, []);
@@ -39,28 +68,25 @@ function Test() {
                             <div className={isActive(index)}>
                                 <div>
                                     <h2>{"Question " + (index + 1) + " : " + item.ques}</h2>
-                                    <div>
-                                        <Button size='lg' block>{"A. " + item.ansA}</Button>
-                                    </div>
-                                    <div>
-                                        <Button size='lg' block>{"B. " + item.ansB}</Button>
-                                    </div>
-                                    <div>
-                                        <Button size='lg' block>{"C. " + item.ansC}</Button>
-                                    </div>
-                                    <div>
-                                        <Button size='lg' block>{"D. " + item.ansD}</Button>
-                                    </div>
-
+                                    <Button color = {isCAT(index,item.ansA)} onClick={() => isChoose(index, item.ansA)} size='lg' block>{"A. " + item.ansA}</Button>{" "}
+                                    <Button color = {isCAT(index,item.ansB)} onClick={() => isChoose(index, item.ansB)} size='lg' block>{"B. " + item.ansB}</Button>{" "}
+                                    <Button color = {isCAT(index,item.ansC)} onClick={() => isChoose(index, item.ansC)} size='lg' block>{"C. " + item.ansC}</Button>{" "}
+                                    <Button color = {isCAT(index,item.ansD)} onClick={() => isChoose(index, item.ansD)} size='lg' block>{"D. " + item.ansD}</Button>{" "}
                                 </div>
                                 <div>
-                                    <Button onClick={() => goPre(index)}>Pre</Button>
-                                    <span>{index + 1}</span>
+                                    <br />
+                                    <Button onClick={() => goPre(index)}>Pre</Button>{" "}
+                                    <span>{index + 1}</span>{" "}
                                     <Button onClick={() => goNext(index)} >Next</Button>
                                 </div>
                             </div>
                         ))
                     }
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button onClick={() => submitTest()}>Submit</Button>
                 </Col>
             </Row>
         </Container>
--- quizapp/src/index.js
+++ quizapp/src/index.js
@@ -3,6 +3,7 @@ import './styles/styles.scss';
 import '../node_modules/@mdi/font/css/materialdesignicons.min.css';
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 
 import App from './App';
