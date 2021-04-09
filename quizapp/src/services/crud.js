8b5af862b27028ba1d6ed7923ceaa57d04f10b40
Nguyen Cong Nhat <51161694+ckan719@users.noreply.github.com>
2021-04-09 10:06:18 +0700

    fix

--- quizapp/src/services/crud.js
+++ quizapp/src/services/crud.js
@@ -1,37 +1,37 @@
 import { DataUsageOutlined } from "@material-ui/icons";
 import HttpRequest from "./http-common";
 const getQuestion = (data) => {
-    return HttpRequest.post("http://192.168.1.4:5000/play-test", data);
+    return HttpRequest.post("http://localhost:5000/play-test", data);
 }
 const getScore = (data) => {
-    return HttpRequest.post("http://192.168.1.4:5000/get-score",data);
+    return HttpRequest.post("http://localhost:5000/get-score",data);
 }
 const getTest = async () => {
-    return HttpRequest.get("http://192.168.1.4:5000/get-test");
+    return HttpRequest.get("http://localhost:5000/get-test");
 }
 const getTestById = (data) => {
-    return HttpRequest.post("http://192.168.1.4:5000/get-test-by-id", data);
+    return HttpRequest.post("http://localhost:5000/get-test-by-id", data);
 }
 const getScoreOfMe = (data) => {
-    return HttpRequest.post("http://192.168.1.4:5000/score-of-me", data);
+    return HttpRequest.post("http://localhost:5000/score-of-me", data);
 }
 const getScoreOfTest = (data) => {
-    return HttpRequest.post("http://192.168.1.4:5000/score-of-test", data);
+    return HttpRequest.post("http://localhost:5000/score-of-test", data);
 }
 const login = (data) => {
-    return HttpRequest.post("http://192.168.1.17:5000/login",data);
+    return HttpRequest.post("http://localhost:5000/login",data);
 }
 
 
 
-const addTest = "http://192.168.1.4:5000/create-test";
-const getTests = "http://192.168.1.4:5000/get-test";
+const addTest = "http://localhost:5000/create-test";
+const getTests = "http://localhost:5000/get-test";
 
 
-const getQuestions="http://192.168.1.4:5000/play-test";
-const deleteQuestion = "http://192.168.1.4:5000/delete-question";
-const updateQuestion = "http://192.168.1.4:5000/update-question";
-const addQuestion = "http://192.168.1.4:5000/create-question";
+const getQuestions="http://localhost:5000/play-test";
+const deleteQuestion = "http://localhost:5000/delete-question";
+const updateQuestion = "http://localhost:5000/update-question";
+const addQuestion = "http://localhost:5000/create-question";
 
 
 
