973b4f7c48822c2ac8e2ec178b50ff19c26e620b
Nguyễn Trọng Hưng <52274610+nguyenhung76@users.noreply.github.com>
2021-04-16 15:35:22 +0700

    Update crud.js

--- quizapp/src/services/crud.js
+++ quizapp/src/services/crud.js
@@ -49,11 +49,5 @@ const getTests = "http://192.168.1.22:5000/get-test";
 const getTestId = "http://192.168.1.22:5000/get-test-by-id";
 const updateTest = "http://192.168.1.22:5000/update-test";
 
-const getUser = "http://192.168.1.17:5000/get-account-by-id";
-const updateUser = "http://192.168.1.17:5000/update-info";
-const addTest = "http://192.168.1.17:5000/create-test";
-const getTests = "http://192.168.1.17:5000/get-test";
-const getTestId = "http://192.168.1.17:5000/get-test-by-id";
-
 
 export default {updateTest, getTestId, getTestByIdTest, getUser, updateUser, register, getInfoAccountById, searchTest , addQuestion, updateQuestion, deleteQuestion, getQuestions, getQuestion, getScore, getTest, getTestById, getScoreOfMe, getScoreOfTest, login, addTest, getTests };
